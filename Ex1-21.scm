56e8d06c97561c672bc96d11c221145290903fc7
Mark Langer <7421898+marklanger@users.noreply.github.com>
2020-11-22 21:25:29 -0800

    Add written notes from 1-13 as well as 1-21 and 1-22

---
+++ Ex1-21.scm
@@ -0,0 +1,25 @@
+#lang racket
+
+(require sicp)
+
+(define (square x)
+  (* x x))
+
+(define (smallest-divisor n)
+  (find-divisor n 2))
+
+(define (find-divisor n test-divisor)
+  ;; can stop if you reach n, or more specifically, half-way to n
+  (cond ((> (square test-divisor) n)
+	 n)
+	((divides? test-divisor n)
+	 test-divisor)
+	(else (find-divisor n (+ test-divisor 1)))))
+
+(define (divides? a b)
+  (= (remainder b a) 0))
+
+
+(smallest-divisor 199)    ;; 199
+(smallest-divisor 1999)   ;; 1999
+(smallest-divisor 19999)  ;; 7
